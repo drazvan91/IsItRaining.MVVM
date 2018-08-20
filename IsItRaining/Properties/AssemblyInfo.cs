82041652dfe536e2c7c421730e0de7617d2f5968
Răzvan Dragomir <razvan.dragomir@tss-yonder.com>
2018-08-20 20:14:02 +0300

    Released v1.1.0

--- IsItRaining/Properties/AssemblyInfo.cs
+++ IsItRaining/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IsItRaining")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Is It Raining WPF")]
+[assembly: AssemblyCopyright("Copyright © Razvan Dragomir 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
